3eeceab2a98dabfc2c407712afbc493a7b83833b
John McAvey <john@mcavey.net>
2015-10-28 19:30:08 -0500

    LOLJK NEEDED OVERRIDE

--- Liftracker/HistoryTableViewController.swift
+++ Liftracker/HistoryTableViewController.swift
@@ -80,7 +80,7 @@ class HistoryTableViewController: UITableViewController {
         return ""
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 
