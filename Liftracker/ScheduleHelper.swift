0af9be4f5da420f39a9a30d36a78122a4ec70f83
John McAvey <mcaveyjc@gmail.com>
2016-09-18 21:46:57 -0500

    Cleanup

--- Liftracker/ScheduleHelper.swift
+++ Liftracker/ScheduleHelper.swift
@@ -24,31 +24,6 @@ class ScheduleHelper {
 
             return calendar.date(byAdding: .day, value: daysToNext, to: date, options: .init(rawValue: 0)) ?? Date()
         }
-//        if let today_calendar = calendar?.component(.weekday, from: date) {
-//            
-//            if let today = Day.fromDayComponent(component: today_calendar) {
-//                
-//                let daysToTravel = day.rawValue - today.rawValue
-//                var daysComponent = DateComponents()
-//                daysComponent.day = 0
-//                daysComponent.month = 0
-//                daysComponent.year = 0
-//            
-//                if daysToTravel > 0 {
-//                    daysComponent.day = daysToTravel
-//                } else {
-//                    let toWeekEnd = Day.Saturday.rawValue - day.rawValue
-//                
-//                    
-//                    // Jump to the weekend, then to the next time this day occurs
-//                    daysComponent.day = toWeekEnd + day.rawValue
-//                }
-//            
-//                return calendar?
-//                    .date(byAdding: daysComponent, to: date, options: .init(rawValue: 0))
-//                    ?? Date()
-//            }
-//        }
         
         return Date()
     }
@@ -62,58 +37,4 @@ enum Day: Int {
     case Thursday = 5
     case Friday = 6
     case Saturday = 7
-    
-    static func fromDayComponent(component: Int) -> Day? {
-        
-        switch component {
-        case CalendarDays.Sunday.rawValue:
-            return .Sunday
-        case CalendarDays.Monday.rawValue:
-            return .Monday
-        case CalendarDays.Teusday.rawValue:
-            return .Teusday
-        case CalendarDays.Wednesday.rawValue:
-            return .Wednesday
-        case CalendarDays.Thursday.rawValue:
-            return .Thursday
-        case CalendarDays.Friday.rawValue:
-            return .Friday
-        case CalendarDays.Saturday.rawValue:
-            return .Saturday
-        default:
-            NSLog("Unkown weekday: \(component)")
-            return nil
-        }
-    }
-    
-    func toDayComponent() -> CalendarDays {
-        
-        switch self {
-        case .Sunday:
-            return .Sunday
-        case .Monday:
-            return .Monday
-        case .Teusday:
-            return .Teusday
-        case .Wednesday:
-            return .Wednesday
-        case .Thursday:
-            return .Thursday
-        case .Friday:
-            return .Friday
-        case .Saturday:
-            return .Saturday
-        }
-    }
-}
-
-enum CalendarDays : Int {
-    
-    case Sunday = 4
-    case Monday = 3
-    case Teusday = 2
-    case Wednesday = 1
-    case Thursday = 7
-    case Friday = 6
-    case Saturday = 5
 }
