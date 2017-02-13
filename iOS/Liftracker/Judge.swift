4da3b46115e91875c57221acec69ead395a9dc51
John McAvey <mcaveyjc@gmail.com>
2017-02-12 23:38:51 -0600

    Judging now uses promises

--- iOS/Liftracker/Judge.swift
+++ iOS/Liftracker/Judge.swift
@@ -7,11 +7,11 @@
 //
 
 import Foundation
-
+import PromiseKit
 /**
  The judge class is responsible for evaluating some combination of data and producing a score for it.  The Judge is allowed to consider data as relative or absolute.  There are no data accessors around judging, only the requirement that by the end you produce some integer value.  If possible, you can provide a maximum and minimum value to give context to the score.
 */
-@objc protocol Judge {
+public protocol Judge {
     
     /**
      This function is used to distinguish judges in the judge pool.  This will be used to cache and update the judges results.
@@ -30,5 +30,5 @@ import Foundation
      
      - parameter completionHandler: Display the result of this calculation.
     */
-    func score(completionHandler: (Double) -> Void)
+    func score() -> Promise<Double>
 }
--- iOS/Liftracker/JudgePool.swift
+++ iOS/Liftracker/JudgePool.swift
@@ -14,38 +14,17 @@ import Foundation
 class JudgePool: NSObject {
     
     /**
-     Singleton instance which is lazily loaded.  Each time the class is instantiated it reloads all the Judge classes in the current execution context.  This is non-trivially expensive (O(n) best case on the number of classes in the project).  To minimize this cost, perform this operation once the first time it's needed.  
+     Singleton instance which is lazily loaded.
     */
     static let shared = JudgePool()
     
     private var judges: [String:Judge.Type]
     
     /**
-        Uses reflection to gather all the Judge classes available in the running project.
     */
     override init() {
         self.judges = [:]
         super.init()
-        
-        log.info("Creating Judge cache.")
-        log.verbose("Retrieving Judge classes")
-        
-        let foundClasses = ReflectionUtil.getImplementing( Judge.self )
-        
-        if let classes = foundClasses as? [Judge.Type] {
-            
-            log.verbose("Retrieved Judge Classes")
-            log.debug("Retrieved \(classes.count) judges")
-            log.debug("Judges: \(classes)")
-            
-            classes.forEach { judge in
-                judges[judge.identifier()] = judge
-            }
-            
-        } else {
-            log.error("Failed to retrieve judge classes")
-            log.error("Judge classes found were the wrong type: \(foundClasses)")
-        }
     }
     
     /**
@@ -56,7 +35,6 @@ class JudgePool: NSObject {
      - Returns: A judge with a matching identifer and expected type or nil if the type/identifier are mismatched
     */
     func getJudge<T: Judge>(forIdentifier id: String) -> T? {
-        log.info("Retrieving Judge")
         log.debug("Retrieving judge with identifier: \(id)")
         let temp = self.judges
         let value = temp[id]
